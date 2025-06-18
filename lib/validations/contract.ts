0111d1ac7d4132634636992c181b5b0b5acdda6b
AbuAli85 <chairman@falconeyegroup.net>
2025-06-18 10:34:05 +0400

    chore: allow file validation on server

--- lib/validations/contract.ts
+++ lib/validations/contract.ts
@@ -21,12 +21,19 @@ const isBrowser = typeof window !== "undefined" && typeof File !== "undefined"
 const fileSchemaOptional = z
   .any()
   .refine(
-    (file) => !file || (isBrowser && file instanceof File && file.size <= MAX_FILE_SIZE),
+    (file) =>
+      !file ||
+      (isBrowser
+        ? file instanceof File && file.size <= MAX_FILE_SIZE
+        : file.size <= MAX_FILE_SIZE),
     `Max file size is 5MB.`,
   )
   .refine(
     (file) =>
-      !file || (isBrowser && file instanceof File && ACCEPTED_IMAGE_TYPES.includes(file.type)),
+      !file ||
+      (isBrowser
+        ? file instanceof File && ACCEPTED_IMAGE_TYPES.includes(file.type)
+        : ACCEPTED_IMAGE_TYPES.includes(file.type)),
     ".jpg, .jpeg, .png, .webp, and .pdf files are accepted.",
   )
   .optional()
