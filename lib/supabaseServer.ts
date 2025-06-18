1bc11a6b43222c8e1121de48b07917e9551287a6
AbuAli85 <chairman@falconeyegroup.net>
2025-06-18 10:49:21 +0400

    refactor: configure supabase server client

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,10 +1,24 @@
+import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-import { createServerComponentClient as createClient } from '@supabase/ssr'
 import type { Database } from '@/types/supabase'
 
-export const createServerComponentClient = () => {
+export function createServerComponentClient() {
   const cookieStore = cookies()
-  return createClient<Database>({
-    cookies: () => cookieStore,
-  })
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options) {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options) {
+          cookieStore.delete({ name, ...options })
+        },
+      },
+    },
+  )
 }
