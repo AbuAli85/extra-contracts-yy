92dc7d1eb5298a709fc42d0125c47575647e1081
AbuAli85 <chairman@falconeyegroup.net>
2025-06-18 23:33:00 +0400

    refactor: move global error page

--- error.tsx
+++
@@ -1,4 +0,0 @@
-// Re-export the global error page from the app directory using the alias
-// defined in tsconfig.json. This avoids runtime resolution issues when Next.js
-// attempts to load the module using a relative specifier.
-export { default } from "@/app/error";
