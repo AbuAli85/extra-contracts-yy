c8d4f5450d0f5b4e8c1ab6c96dfd08d977e7a106
AbuAli85 <chairman@falconeyegroup.net>
2025-06-19 20:20:37 +0400

    Add localized login route

---
+++ app/[locale]/login/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from "@/components/auth-form"
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen bg-background flex flex-col items-center justify-center p-4">
+      <AuthForm />
+    </div>
+  )
+}
