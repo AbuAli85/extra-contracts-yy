a8c9902a4ad42644ee46a82385b10997bb1c42f3
AbuAli85 <chairman@falconeyegroup.net>
2025-06-29 23:02:09 +0400

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,24 +1,20 @@
 import { createServerClient } from "@supabase/ssr"
 import { cookies } from "next/headers"
 import type { Database } from "@/types/supabase"
+import 'server-only' // Mark this module as server-only
 
 export const createClient = () => {
   const cookieStore = cookies()
-
-  return createServerClient<Database>(process.env.PUBLIC_SUPABASE_URL!, process.env.PUBLIC_SUPABASE_ANON_KEY!, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll()
+  
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
       },
-      setAll(cookiesToSet) {
-        try {
-          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
-        } catch {
-          // The `setAll` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
-        }
-      },
-    },
-  })
+    }
+  )
 }
