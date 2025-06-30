28f3f0a6b3e94f7ebceae9401316539cf8beb9cf
fahad alamri <luxsess2001@hotmail.com>
2025-06-30 17:28:55 +0400

    updated

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -5,7 +5,6 @@ import type { Database } from "@/types/supabase"
 
 export function createClient() {
   const cookieStore = cookies()
-
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -17,11 +16,7 @@ export function createClient() {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          } catch {}
         },
       },
     },
