4d612a0eff90ec9d201d7dbb9ed0ae87fdfc6cde
fahad alamri <luxsess2001@hotmail.com>
2025-07-09 17:52:43 +0400

    updated

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,13 +1,20 @@
 import "../globals.css"
 import type React from "react"
 import ClientLayout from "../client-layout"
+import { NextIntlClientProvider } from "next-intl"
+import { getMessages } from "next-intl/server"
 
-export default function LocaleLayout({ 
-  children, 
-  params 
-}: { 
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
   children: React.ReactNode
   params: { locale: string }
 }) {
-  return <ClientLayout params={params}>{children}</ClientLayout>
+  const messages = await getMessages()
+  return (
+    <NextIntlClientProvider locale={params.locale} messages={messages}>
+      <ClientLayout params={params}>{children}</ClientLayout>
+    </NextIntlClientProvider>
+  )
 }
